91ba7b7fd6b6eece6f5b4fc7d3c4ece167770ddf
mac_vbox <maoze365@gmail.com>
2015-07-18 10:48:25 +0800

    xxx

--- app/views.py
+++ app/views.py
@@ -2,17 +2,14 @@
 
 from flask import Flask, make_response, render_template, flash, redirect, session, url_for, request, g, jsonify
 from app import app
-from urllib import urlopen, urlencode
+from urllib import urlopen, urlencode, quote_plus
 import requests
 
 from itertools import chain
 IPN_URLSTRING = 'https://www.sandbox.paypal.com/cgi-bin/webscr'
 IPN_VERIFY_EXTRA_PARAMS = (('cmd', '_notify-validate'),)
+from itertools import chain
 
-
-@app.route('/')
-def index():
-	return "Hello Paypal"
 def ordered_storage(f):
     import werkzeug.datastructures
     import flask
@@ -24,23 +21,19 @@ def ordered_storage(f):
 @app.route('/paypal/', methods=['POST'])
 @ordered_storage
 def paypal_webhook():
-    
     #probably should have a sanity check here on the size of the form data to guard against DoS attacks
-    received_args = chain(IPN_VERIFY_EXTRA_PARAMS, request.form.iteritems())
-    print request.form
-    verify_string = '&'.join(('%s=%s' % (param, urllib.quote(value.decode('string_escape'))) for param, value in received_args))
-    print verify_string
-    
-    response = requests.post(IPN_URLSTRING, data=verify_string)
-    #response = urlopen(IPN_URLSTRING+'?'+verify_string)
+    verify_args = chain(IPN_VERIFY_EXTRA_PARAMS, request.form.iteritems())
+    verify_string = '&'.join(('%s=%s' % (param, quote_plus(value.encode('utf-8'), safe=':/')) for param, value in verify_args))
+    #req = Request(verify_string)
+    response = urlopen(IPN_URLSTRING+'?'+verify_string)
     status = response.read()
-    print "status is %s"%status
+    print status
     if status == 'VERIFIED':
         print "PayPal transaction was verified successfully."
         # Do something with the verified transaction details.
         payer_email =  request.form.get('payer_email')
         print "Pulled {email} from transaction".format(email=payer_email)
     else:
-        print 'Paypal IPN string did not validate'
+         print 'Paypal IPN string {arg} did not validate'.format(arg=verify_string)
 
     return jsonify({'status':'complete'})
